8f00b155d6142056ef2fa081eae983d6c89e8f8b
Alejandro Martir <hola@alemartir.com>
2024-10-17 01:06:55 -0600

    Add page info

--- src/app/(main)/contacto/page.jsx
+++ src/app/(main)/contacto/page.jsx
@@ -35,8 +35,20 @@ export default function ContactPage() {
     <div className="container mx-auto px-4 py-2 md:px-0">
       <h1>Contacto</h1>
 
-      <div className="grid grid-cols-12 gap-4 md:gap-12">
-        <div className="col-span-12 md:col-span-6">
+      <div className="flex flex-col md:flex-row items-center justify-center gap-4 md:gap-12">
+        <div className="w-full md:w-1/2">
+          <h2 className="text-6xl md:text-7xl font-extrabold mb-8">
+            ¿Hablamos?
+          </h2>
+          <p className="mb-16 md:mb-0">
+            ¿No quieres que tu marca sea igual a las demás? ¿Te gustaría mejorar
+            la que ya tienes o crearla de cero? Estás en la web correcta. Llena
+            el formulario y así, podremos tener una llamada de contacto y te
+            prepararé un presupuesto adaptado a lo que necesites.
+          </p>
+        </div>
+
+        <div className="w-full md:w-1/2">
           <ContactForm />
         </div>
       </div>
